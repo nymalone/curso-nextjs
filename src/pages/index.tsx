81e847d84c83614b17681c0ab16c2a5440569815
Nykolle Malone <malone.nykolle@gmail.com>
2020-11-08 11:16:20 -0300

    client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/Pages/Home';
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3334/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data)
+      })
+    })
+  }, []);
+  
   return (
    <div>
-     <Title>Hello</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
    </div>
   )
 }
