c86277a8d37e04700800e2693fdadf5ce9af7162
Nykolle Malone <malone.nykolle@gmail.com>
2020-11-10 08:37:40 -0300

    utilizando sdk

--- src/pages/catalog/categories/[slug].tsx
+++ src/pages/catalog/categories/[slug].tsx
@@ -1,17 +1,17 @@
+import { client } from '@/lib/prismic';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { useRouter } from 'next/router'
-
-
-interface IProduct {
-  id: string;
-  title: string;
-}
+import { useRouter } from 'next/router';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import Link from 'next/link';
+import {Document} from 'prismic-javascript/types/documents';
 
 interface ICategoryProps { 
-  products: IProduct[];
+  category: Document;
+  products: Document[];
 }
 
-export default function Category({ products }: ICategoryProps) {
+export default function Category({ category, products }: ICategoryProps) {
   const router = useRouter();
 
   if (router.isFallback) {
@@ -20,14 +20,18 @@ export default function Category({ products }: ICategoryProps) {
 
   return (
     <div>
-      <h1>{router.query.slug}</h1>
+      <h1>{PrismicDOM.RichText.asText(category.data.title)}</h1>
 
       <ul>
-          {products.map(recommendedProduct => {
+          {products.map(product => {
             return (
-              <li key={recommendedProduct.id}>
-                {recommendedProduct.title}
-              </li>
+              <li key={product.id}>
+              <Link href={`/catalog/products/${product.uid}`}>
+                <a>
+                {PrismicDOM.RichText.asText(product.data.title)}
+                </a>
+              </Link>
+            </li>
             )
           })}
         </ul>
@@ -36,12 +40,13 @@ export default function Category({ products }: ICategoryProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch(`http://localhost:3334/categories`)
-  const categories = await response.json();
+  const categories = await client().query([
+    Prismic.Predicates.at('document.type', 'category')
+  ])
 
-  const paths = categories.map(category => {
+  const paths = categories.results.map(category => {
     return {
-      params: { slug: category.id}
+      params: { slug: category.uid}
     }
   })
 
@@ -55,11 +60,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<ICategoryProps> = async (context) => {
   const { slug } = context.params;
 
-  const response = await fetch(`http://localhost:3334/products?category_id=${slug}`)
-  const products = await response.json();
+  const category = await client().getByUID('category', String(slug), {});
+
+  const products = await client().query([
+    Prismic.Predicates.at('document.type', 'product'),
+    Prismic.Predicates.at('my.product.category', category.id)
+  ])
+
   return {
     props: {
-      products,
+      category,
+      products: products.results,
     },
     revalidate: 60 
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 //import { useEffect, useState } from 'react';
-import { GetServerSideProps } from 'next';
-import { Title } from '@/styles/Pages/Home';
-import SEO from '@/components/SEO';
-
-interface IProduct {
-  id: string;
-  title: string;
-}
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { Title } from "@/styles/Pages/Home";
+import SEO from "@/components/SEO";
+import { client } from "@/lib/prismic";
+import Prismic from "prismic-javascript";
+import PrismicDOM from 'prismic-dom';
+import { Document } from "prismic-javascript/types/documents";
 
 interface IHomeProps {
-  recommendedProducts: IProduct[]
+  recommendedProducts: Document[];
 }
 
 export default function Home({ recommendedProducts }: IHomeProps) {
- // const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+  // const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
 
   // CLIENT SIDE FETCHING -> uso quando eu não preciso que esteja nos motores de busca, infos que eu não preciso indexar; só vai ser disparada no browser
   // useEffect(() => {
@@ -24,20 +24,15 @@ export default function Home({ recommendedProducts }: IHomeProps) {
   //   })
   // }, []);
 
-  console.log(process.env.NEXT_PUBLIC_API_URL)
-  
-  async function handleSum() {
-    const math = (await import('../lib/math')).default;
+  console.log(process.env.NEXT_PUBLIC_API_URL);
 
-    alert(math.sum(2, 5));
-  }
   return (
-   <div>
-     <SEO 
-      title="DevCommerce, you best choice in the internet" 
-      image="olivia.jpeg"
-      shouldExcludeTitleSuffix
-    />
+    <div>
+      <SEO
+        title="DevCommerce, you best choice in the internet"
+        image="olivia.jpeg"
+        shouldExcludeTitleSuffix
+      />
 
       <section>
         <Title>Products</Title>
@@ -46,37 +41,39 @@ export default function Home({ recommendedProducts }: IHomeProps) {
           {recommendedProducts.map(recommendedProduct => {
             return (
               <li key={recommendedProduct.id}>
-                {recommendedProduct.title}
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                  {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
               </li>
-            )
+            );
           })}
         </ul>
-
-        <button onClick={handleSum}>Sum!</button>
       </section>
-   </div>
-  )
+    </div>
+  );
 }
 
-// SERVER SIDE RENDERING 
-// vai buscar algum tipo de dado e vai retornar para o componente através de propriedades 
-// então eu posso acessar qualquer coisa que é retornado dessa função nas minhas props 
+// SERVER SIDE RENDERING
+// vai buscar algum tipo de dado e vai retornar para o componente através de propriedades
+// então eu posso acessar qualquer coisa que é retornado dessa função nas minhas props
 export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
-     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`)
-     const recommendedProducts = await response.json();
+  // esse client() é o que criei no prismic
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at("document.type", "product"), // vai me retornar todos os docs lá do prismic onde o tipo for igual a produto
+  ]);
 
-     return {
-      props: {
-        recommendedProducts
-      }
-     }
-}
+  return {
+    props: {
+      recommendedProducts: recommendedProducts.results,
+    },
+  };
+};
 
-// não devo usar o getServerSideProps para toda ocasião! só vou utilizar quando eu precisar uma informação que é dinãmica ("listagem api") 
+// não devo usar o getServerSideProps para toda ocasião! só vou utilizar quando eu precisar uma informação que é dinãmica ("listagem api")
 // que precisa estar disponível e indexadas para os motores de busca!
-// pq se eu ficar utilizando o getServerSideProps para qualquer coisa a tela toda pode demorar para aparecer, 
+// pq se eu ficar utilizando o getServerSideProps para qualquer coisa a tela toda pode demorar para aparecer,
 // o que chamamos de TTFB (time to first bite) que se demorar não é uma exp boa para o usuário
 
-// TTFB com o client side fetching é muito menor, para o usuário é bom, mas para o motor de busca não 
-
-
+// TTFB com o client side fetching é muito menor, para o usuário é bom, mas para o motor de busca não
